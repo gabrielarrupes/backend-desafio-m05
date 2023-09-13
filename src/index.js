23a075fd2af6cb67785b279a632c4159593d25c0
Eduardo Mendes <eduardortm0@gmail.com>
2023-09-13 14:00:03 -0300

    :wrench: criação da pasta e arquivo de rotas
    configuração inicial do arquivo de rotas
    
    importação de rotas no arquivo index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 
+const rotas = require("./routes");
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use(router);
 
 const port = process.env.PORT || 3001;
 
---
+++ src/routes/router.js
@@ -0,0 +1,5 @@
+const express = require("express");
+
+router = express();
+
+module.exports = router;
