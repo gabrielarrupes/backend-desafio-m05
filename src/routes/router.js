c516c466894aca0a90dfea3fadf9481c62b18d51
Eduardo Mendes <eduardortm0@gmail.com>
2023-09-14 10:48:13 -0300

    :heavy_plus_sign:  build: instalaçao jwt

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,9 +3,13 @@ const express = require("express");
 const UserRegistrationValidation = require("../middleware/UserRegistrationValidation");
 
 const { postUser } = require("../controllers/user");
+const Login = require("../controllers/Login");
+
 const { userSchema } = require("../schemas/userSchema");
 
 router = express();
+
+router.post("/Login", Login);
 
 router.post("/signUp", UserRegistrationValidation(userSchema), postUser);
 
