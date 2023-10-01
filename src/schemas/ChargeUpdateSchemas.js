720bf9cff92c33bcd7fe992676c5cd2b007d1928
Eduardo Mendes <eduardortm0@gmail.com>
2023-10-01 18:37:18 -0300

    :wrench: Atualização do chargeSchema
    Inclusão do campo idcustomer para ser validado

--- src/schemas/ChargeUpdateSchemas.js
+++ src/schemas/ChargeUpdateSchemas.js
@@ -1,46 +1,29 @@
 const joi = require("joi");
 
 const chargeUpdateSchemas = joi.object({
+  description: joi.string().required().messages({
+    "any.required": "O campo descrição é obrigatório",
+    "string.empty": "A descrição não pode estar vazia",
+  }),
 
-    description: joi
-        .string()
-        .required()
-        .messages({
-            "any.required": "O campo descrição é obrigatório",
-            "string.empty": "A descrição não pode estar vazia",
-        }),
+  duedate: joi.string().required().messages({
+    "any.required": "O campo data de vencimento é obrigatório",
+    "string.empty": "A data não pode estar vazia",
+  }),
 
-    duedate: joi
-        .string()
-        .required()
-        .messages({
-            "any.required": "O campo data de vencimento é obrigatório",
-            "string.empty": "A data não pode estar vazia",
-        }),
+  value: joi.number().positive().required().messages({
+    "any.required": "O campo valor é obrigatório",
+    "number.empty": "O valor não pode estar vazio",
+    "number.base": "O campo valor precisa ser um número",
+    "number.positive": "O campo valor precisa ser um número positivo",
+  }),
 
-    value: joi
-        .number()
-        .positive()
-        .required()
-        .messages({
-            "any.required": "O campo valor é obrigatório",
-            "number.empty": "O valor não pode estar vazio",
-            "number.base": "O campo valor precisa ser um número",
-            "number.positive": "O campo valor precisa ser um número positivo"
-        }),
+  status: joi.boolean().required().messages({
+    "any.required": "O campo status é obrigatório",
+    "boolean.empty": "O status não pode estar vazio",
+  }),
 
-
-
-    status: joi
-        .boolean()
-        .required()
-        .messages({
-            "any.required": "O campo status é obrigatório",
-            "boolean.empty": "O status não pode estar vazio",
-        }),
-
+  idcustomer: joi.number(),
 });
 
-
-
-module.exports = { chargeUpdateSchemas }
+module.exports = { chargeUpdateSchemas };
