7cfcf9f1f423c3e15d36eb59428580277745eefc
Eduardo Mendes <eduardortm0@gmail.com>
2023-10-04 20:32:12 -0300

    :wrench: Atualizações Básicas

--- src/controllers/Charge.js
+++ src/controllers/Charge.js
@@ -1,134 +1,129 @@
 const connection = require("../services/connection");
 
-
 const postCharge = async (req, res) => {
+  const { idcustomer, value, duedate, status, description } = req.body;
 
-    const {
-        idcustomer,
-        value,
-        duedate,
-        status,
-        description
-    } = req.body;
+  try {
+    const charge = await connection("charge").insert({
+      idcustomer,
+      value,
+      duedate,
+      status,
+      description,
+    });
 
-    try {
-        const charge = await connection("charge").insert({
-            idcustomer,
-            value,
-            duedate,
-            status,
-            description
-        });
-
-        if (!charge) {
-            return res.status(400).json("Não foi possível concluir o cadastro de cobrança");
-        }
-
-        return res.status(201).json("Cobrança cadastrada com sucesso.");
-    } catch (error) {
-        return res.status(500).json({ message: "Erro interno do servidor" });
+    if (!charge) {
+      return res
+        .status(400)
+        .json("Não foi possível concluir o cadastro de cobrança");
     }
+
+    return res.status(201).json("Cobrança cadastrada com sucesso.");
+  } catch (error) {
+    return res.status(500).json({ message: "Erro interno do servidor" });
+  }
 };
 
 const getChargeId = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const chargeId = await connection("charge").where({ idcustomer: id });
+  try {
+    const { id } = req.params;
+    const chargeId = await connection("charge").where({ idcustomer: id });
 
-        if (!chargeId) {
-            return res.status(404).json({ mesagem: "Cobrança não encontrada" })
-        }
-
-        return res.status(200).json(chargeId);
-    } catch (error) {
-        console.log(error);
+    if (!chargeId) {
+      return res.status(404).json({ mesagem: "Cobrança não encontrada" });
     }
-}
 
+    return res.status(200).json(chargeId);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const getCharge = async (req, res) => {
+  try {
+    const charge = await connection("charge")
+      .select("charge.*", "customers.name")
+      .from("charge")
+      .join("customers", "charge.idcustomer", "customers.id");
 
-    try {
-        const charge = await connection("charge").select(
-            'charge.*',
-            'customers.name'
-        )
-            .from('charge')
-            .join('customers', 'charge.idcustomer', 'customers.id');
-
-        if (!charge) {
-            return res.status(404).json("Cobrança não encontrada!");
-        }
-
-        return res.status(200).json(charge);
-    } catch (error) {
-        return res.status(500).json("Erro interno do servidor");
+    if (!charge) {
+      return res.status(404).json("Cobrança não encontrada!");
     }
-}
+
+    return res.status(200).json(charge);
+  } catch (error) {
+    return res.status(500).json("Erro interno do servidor");
+  }
+};
 
 const putCharge = async (req, res) => {
-    const {
+  const { idcustomer, value, duedate, status, description } = req.body;
+  const { id } = req.params;
+  try {
+    const chargeExist = await connection("charge").where({ id }).first();
+    if (!chargeExist) {
+      return res.status(404).json({ message: "Cobrança não encontrada" });
+    }
+    const chargeUpdate = await connection("charge")
+      .where({ id })
+      .update({
         idcustomer,
         value,
         duedate,
         status,
-        description
-    } = req.body
-    const { id } = req.params;
-    try {
-        const chargeExist = await connection("charge").where({ id }).first()
-        if (!chargeExist) {
-            return res.status(404).json({ message: "Cobrança não encontrada" });
-        }
-        const chargeUpdate = await connection("charge").where({ id })
-            .update({
-                idcustomer,
-                value,
-                duedate,
-                status,
-                description
-            }).returning("*");
+        description,
+      })
+      .returning("*");
 
-        if (!chargeUpdate) {
-            return res.status(400).json({ message: "Erro ao atualizar a cobrança" });
-        }
-        return res.status(200).json({ message: "Cobrança atualizada com sucesso!" });
-    } catch (error) {
-
-        return res.status(500).json("Erro interno do servidor");
+    if (!chargeUpdate) {
+      return res.status(400).json({ message: "Erro ao atualizar a cobrança" });
     }
-}
-
+    return res
+      .status(200)
+      .json({ message: "Cobrança atualizada com sucesso!" });
+  } catch (error) {
+    return res.status(500).json("Erro interno do servidor");
+  }
+};
 
 const deleteCharge = async (req, res) => {
-    const { id } = req.params;
-    try {
+  const { id } = req.params;
+  try {
+    const chargeId = await connection("charge").where("id", id).first();
 
-        const chargeId = await connection("charge").where('id', id).first();
-
-        if (!chargeId) {
-            return res.status(404).json({ mesagem: "Cobrança não encontrada" })
-        }
-        if (chargeId.status === true) {
-            return res.status(403).json({ message: 'Não é permitido excluir uma cobrança paga!' });
-        }
-        const currentDate = new Date();
-        const chargeDate = new Date(chargeId.duedate);
-
-        if (chargeDate < currentDate) {
-            return res.status(403).json({ message: 'Não é permitido excluir uma cobrança vencida!' });
-        }
-
-        const removeCharge = await connection("charge").where({ id }).delete();
-
-        if (!removeCharge) {
-            return res.status(400).json({ message: 'Cobrança não excluida' });
-        }
-
-        return res.status(200).json({ message: 'Cobrança excluída com sucesso' });
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json("Erro interno do servidor");
+    if (!chargeId) {
+      return res.status(404).json({ mesagem: "Cobrança não encontrada" });
     }
-}
-module.exports = { postCharge, getCharge, getChargeId, putCharge, deleteCharge }
+    if (chargeId.status === true) {
+      return res
+        .status(403)
+        .json({ message: "Não é permitido excluir uma cobrança paga!" });
+    }
+    const currentDate = new Date();
+    const chargeDate = new Date(chargeId.duedate);
+
+    if (chargeDate < currentDate) {
+      return res
+        .status(403)
+        .json({ message: "Não é permitido excluir uma cobrança vencida!" });
+    }
+
+    const removeCharge = await connection("charge").where({ id }).delete();
+
+    if (!removeCharge) {
+      return res.status(400).json({ message: "Cobrança não excluida" });
+    }
+
+    return res.status(200).json({ message: "Cobrança excluída com sucesso" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json("Erro interno do servidor");
+  }
+};
+module.exports = {
+  postCharge,
+  getCharge,
+  getChargeId,
+  putCharge,
+  deleteCharge,
+};
--- src/schemas/CustomerUpdateSchema.js
+++ src/schemas/CustomerUpdateSchema.js
@@ -1,65 +1,70 @@
 const joi = require("joi");
 
 const customerUpdateSchema = joi.object({
-    name: joi
-        .string()
-        .regex(/^[a-zA-Z\s]+$/)
-        .required()
-        .messages({
-            "string.empty": "O nome não pode estar vazio",
-            "any.required": "O nome é obrigatório",
-            "string.pattern.base": "O nome deve conter apenas letras e espaços",
-        }),
-
-    email: joi.string().email().required().messages({
-        "string.email": "O e-mail deve ter um formato válido, exemplo: email@email.com",
-        "string.empty": "O email não pode estar vazio",
-        "any.required": "O email é obrigatório",
+  name: joi
+    .string()
+    .regex(/^[a-zA-Z\s]+$/)
+    .required()
+    .messages({
+      "string.empty": "O nome não pode estar vazio",
+      "any.required": "O nome é obrigatório",
+      "string.pattern.base": "O nome deve conter apenas letras e espaços",
     }),
 
-    cpf: joi.string().min(11).max(11).pattern(/^\d+$/).required().messages({
-        "string.min": "O CPF deve conter 11 números",
-        "string.pattern.base": "O cpf deve conter apenas números",
-        "string.max": "O CPF deve conter apenas 11 números",
-        "any.required": "O CPF é obrigatório"
+  email: joi.string().email().required().messages({
+    "string.email":
+      "O e-mail deve ter um formato válido, exemplo: email@email.com",
+    "string.empty": "O email não pode estar vazio",
+    "any.required": "O email é obrigatório",
+  }),
+
+  cpf: joi
+    .string()
+    .min(0)
+    .max(11)
+    .pattern(/^(?:\d{11,})?$/)
+    .messages({
+      "string.min": "O CPF deve conter 11 números",
+      "string.pattern.base": "O cpf deve conter apenas números",
+      "string.max": "O CPF deve conter apenas 11 números",
+      "any.required": "O CPF é obrigatório",
     }),
 
-    telephone: joi.string().pattern(/^\d+$/).required().messages({
-        "string.pattern.base": "O telefone deve conter apenas números",
-        "any.required": "O telefone é obrigatório",
-    }),
+  telephone: joi.string().pattern(/^\d+$/).messages({
+    "string.pattern.base": "O telefone deve conter apenas números",
+    "any.required": "O telefone é obrigatório",
+  }),
 
-    logradouro: joi.string().required().messages({
-        "string.empty": "O logradouro não pode estar vazio",
-        "any.required": "O logradouro é obrigatório",
-    }),
+  logradouro: joi.string().required().messages({
+    "string.empty": "O logradouro não pode estar vazio",
+    "any.required": "O logradouro é obrigatório",
+  }),
 
-    complemento: joi.string().required().messages({
-        "string.empty": "O complemento não pode estar vazio",
-        "any.required": "O complemento é obrigatório",
-    }),
+  complemento: joi.string().required().messages({
+    "string.empty": "O complemento não pode estar vazio",
+    "any.required": "O complemento é obrigatório",
+  }),
 
-    cep: joi.string().max(8).pattern(/^\d+$/).required().messages({
-        "string.max": "O cep deve ter no máximo 8 caracteres",
-        "string.pattern.base": "O cep deve conter apenas números",
-        "any.required": "O CEP é obrigatório"
-    }),
+  cep: joi.string().max(8).pattern(/^\d+$/).required().messages({
+    "string.max": "O cep deve ter no máximo 8 caracteres",
+    "string.pattern.base": "O cep deve conter apenas números",
+    "any.required": "O CEP é obrigatório",
+  }),
 
-    bairro: joi.string().required().messages({
-        "string.empty": "O bairro não pode estar vazio",
-        "any.required": "O bairro é obrigatório",
-    }),
+  bairro: joi.string().required().messages({
+    "string.empty": "O bairro não pode estar vazio",
+    "any.required": "O bairro é obrigatório",
+  }),
 
-    cidade: joi.string().required().messages({
-        "string.empty": "A cidade não pode estar vazia",
-        "any.required": "A cidade é obrigatória",
-    }),
+  cidade: joi.string().required().messages({
+    "string.empty": "A cidade não pode estar vazia",
+    "any.required": "A cidade é obrigatória",
+  }),
 
-    estado: joi.string().required().messages({
-        "string.empty": "O estado não pode estar vazio",
-        "any.required": "O estado é obrigatório",
-    }),
+  estado: joi.string().required().messages({
+    "string.empty": "O estado não pode estar vazio",
+    "any.required": "O estado é obrigatório",
+  }),
 });
-
 
 module.exports = { customerUpdateSchema };
